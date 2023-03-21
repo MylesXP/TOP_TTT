361ebfbd36e81372cf2f4c8fdc7f597b30100aec
MylesXP <mylesparkinson12@gmail.com>
2023-03-20 20:13:45 -0700

    Created winning logic for board
    Created winning logic for board based on both player's marks on board. (horizontal, vertical, diagonal)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let currentPlayer = 1;
 let gridItem = document.getElementsByClassName('grid-item');
-
+let gridLine = document.getElementsByClassName('grid-line');
 
 
 
@@ -61,6 +61,8 @@ for (let i = 0; i < gridItem.length; i++){
             }
             changePlayer()
         }
+
+        
     });
 };
 
@@ -71,4 +73,111 @@ function reset () {
     };
     currentPlayer = 1;
     gameBoard.playerBoardCounter.textContent = currentPlayer;
-}
+    x.observe(gridItem[0], {childList: true, subtree: true,
+    attributes: true});
+    
+    x.observe(gridItem[1], {childList: true, subtree: true,
+    attributes: true});
+    
+    x.observe(gridItem[2], {childList: true, subtree: true,
+    attributes: true});
+    
+    x.observe(gridItem[3], {childList: true, subtree: true,
+    attributes: true});
+    
+    x.observe(gridItem[4], {childList: true, subtree: true,
+    attributes: true});
+    
+    x.observe(gridItem[5], {childList: true, subtree: true,
+    attributes: true});
+    
+    x.observe(gridItem[6], {childList: true, subtree: true,
+    attributes: true});
+    
+    x.observe(gridItem[7], {childList: true, subtree: true,
+    attributes: true});
+    
+    x.observe(gridItem[8], {childList: true, subtree: true,
+    attributes: true});
+};
+
+//Winning Logic
+// if (gridLine[0].children[0].textContent == player1.mark && gridLine[0].children[1] == player1.mark && gridLine[0].children[2] == player1.mark) {
+//     console.log('won')
+// }
+
+// var x = new MutationObserver(function (e) {
+//     if (gridLine[0].children[0].textContent == player1.mark && gridLine[0].children[1] == player1.mark && gridLine[0].children[2] == player1.mark){
+//         console.log('hello')
+//     }
+//   });
+  
+//   x.observe(gridLine[0].children[0], { childList: true});
+
+
+var x = new MutationObserver(e => {
+    //Straight Across
+    if (gridItem[0].textContent == player1.mark && gridItem[1].textContent == player1.mark && gridItem[2].textContent == player1.mark || gridItem[0].textContent == player2.mark && gridItem[1].textContent == player2.mark && gridItem[2].textContent == player2.mark ){
+        console.log(`PLAYER ${gridItem[0].textContent == 'o' ? 2 : 1} WINS!!!`);
+        x.disconnect();
+    }
+    else if (gridItem[3].textContent == player1.mark && gridItem[4].textContent == player1.mark && gridItem[5].textContent == player1.mark || gridItem[3].textContent == player2.mark && gridItem[4].textContent == player2.mark && gridItem[5].textContent == player2.mark){
+        console.log(`PLAYER ${gridItem[3].textContent == 'o' ? 2 : 1} WINS!!!`);
+        x.disconnect();
+    }
+    else if (gridItem[6].textContent == player1.mark && gridItem[7].textContent == player1.mark && gridItem[8].textContent == player1.mark || gridItem[6].textContent == player2.mark && gridItem[7].textContent == player2.mark && gridItem[8].textContent == player2.mark){
+        console.log(`PLAYER ${gridItem[6].textContent == 'o' ? 2 : 1} WINS!!!`);
+        x.disconnect();
+    }
+
+    //Straight Down
+    if (gridItem[0].textContent == player1.mark && gridItem[3].textContent == player1.mark && gridItem[6].textContent == player1.mark || gridItem[0].textContent == player2.mark && gridItem[3].textContent == player2.mark && gridItem[6].textContent == player2.mark){
+        console.log(`PLAYER ${gridItem[0].textContent == 'o' ? 2 : 1} WINS!!!`);
+        x.disconnect();
+    }
+    else if (gridItem[1].textContent == player1.mark && gridItem[4].textContent == player1.mark && gridItem[7].textContent == player1.mark || gridItem[1].textContent == player2.mark && gridItem[4].textContent == player2.mark && gridItem[7].textContent == player2.mark){
+        console.log(`PLAYER ${gridItem[1].textContent == 'o' ? 2 : 1} WINS!!!`);
+        x.disconnect();
+    }
+    else if (gridItem[2].textContent == player1.mark && gridItem[5].textContent == player1.mark && gridItem[8].textContent == player1.mark || gridItem[2].textContent == player2.mark && gridItem[5].textContent == player2.mark && gridItem[8].textContent == player2.mark){
+        console.log(`PLAYER ${gridItem[2].textContent == 'o' ? 2 : 1} WINS!!!`);
+        x.disconnect();
+    }
+
+    // //Diagonal
+    if (gridItem[0].textContent == player1.mark && gridItem[4].textContent == player1.mark && gridItem[8].textContent == player1.mark || gridItem[0].textContent == player2.mark && gridItem[4].textContent == player2.mark && gridItem[8].textContent == player2.mark ){
+        console.log(`PLAYER ${gridItem[0].textContent == 'o' ? 2 : 1} WINS!!!`);
+        x.disconnect();
+    }
+    else if (gridItem[2].textContent == player1.mark && gridItem[4].textContent == player1.mark && gridItem[6].textContent == player1.mark || gridItem[2].textContent == player2.mark && gridItem[4].textContent == player2.mark && gridItem[6].textContent == player2.mark){
+        console.log(`PLAYER ${gridItem[2].textContent == 'o' ? 2 : 1} WINS!!!`);
+        x.disconnect();
+    }
+});
+
+x.observe(gridItem[0], {childList: true, subtree: true,
+attributes: true});
+
+x.observe(gridItem[1], {childList: true, subtree: true,
+attributes: true});
+
+x.observe(gridItem[2], {childList: true, subtree: true,
+attributes: true});
+
+x.observe(gridItem[3], {childList: true, subtree: true,
+attributes: true});
+
+x.observe(gridItem[4], {childList: true, subtree: true,
+attributes: true});
+
+x.observe(gridItem[5], {childList: true, subtree: true,
+attributes: true});
+
+x.observe(gridItem[6], {childList: true, subtree: true,
+attributes: true});
+
+x.observe(gridItem[7], {childList: true, subtree: true,
+attributes: true});
+
+x.observe(gridItem[8], {childList: true, subtree: true,
+attributes: true});
